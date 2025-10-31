813786df81136a3080aa162981b1fc7b22d762c3
Strider Jones <stridera@gmail.com>
2025-10-31 00:08:54 -0700

    Update Schema

--- packages/db/prisma/migrations/20250915062706_add_quantity_to_equipment_set_items/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropIndex
-DROP INDEX "public"."equipment_set_items_equipmentSetId_slot_key";
-
--- AlterTable
-ALTER TABLE "public"."equipment_set_items" ADD COLUMN     "quantity" INTEGER NOT NULL DEFAULT 1,
-ALTER COLUMN "slot" DROP NOT NULL;
